320d1361d39ee19a343348718e073dd6e9517bf5
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-30 15:35:56 +0300

    add books to library

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Hi from app.js");
+let myLibrary = [];
+
+const add = document.getElementById("add");
+add.addEventListener("click", addBookToLibrary);
+
+function Book(book, author, pages, read) {
+	this.book = book;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+function addBookToLibrary() {
+	const book = document.getElementById("book").value;
+	const author = document.getElementById("author").value;
+	const pages = document.getElementById("pages").value;
+	const read = document.getElementById("read").value;
+	if (book === "" || author === "" || pages === "" || read === "") {
+		return alert("Please fill in all fields");
+	}
+	if (isNaN(pages)) {
+		return alert("Please enter a number for pages");
+	}
+
+	const newBook = new Book(book, author, pages, read);
+	myLibrary.push(newBook);
+	console.log(myLibrary);
+}
